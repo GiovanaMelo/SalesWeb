e92390c47ed76fc196211a911a95772cc239b2dc
Giovana <giovanamelomr@gmail.com>
2021-02-11 17:14:38 -0300

    Update seller and custom service exception

---
+++ SalesWebMvc/Services/Exeptions/DbUpdatConcurrencyException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Services.Exeptions
+{
+    public class DbUpdatConcurrencyException : ApplicationException
+    {
+        public DbUpdatConcurrencyException(string message) : base(message)
+        {
+
+        }
+    }
+}
